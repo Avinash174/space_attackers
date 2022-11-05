e007983ed2559752181030e88f26f1f8c18e7dd9
Avinash175 <avinashmagar15@gmail.com>
2022-11-06 00:05:04 +0530

    New Changes Adding

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,18 +33,63 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  double  shipX =0.0,shipY=0.0;
+  double maxHeight=0.0;
+  double intialPosition=0.0;
+  double velocity=2.9;
+  double time =0.0;
+  double gravity=-4.9;
+  bool isGameRunning =false;
+
+  void startGame(){
+    isGameRunning=true;
+    Timer.periodic(Duration(milliseconds: 30), (timer) {
+      time = time+ 0.02;
+      setState(() {
+        maxHeight = velocity * time+ gravity *time * time;
+        shipY = intialPosition -maxHeight;
+      });
+    });
+  }
+
+  void onJump(){
+   setState(() {
+     time =0;
+     intialPosition=shipY;
+   });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
+      body: GestureDetector(
+        onTap: isGameRunning?onJump:startGame,
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
             image: DecorationImage(
                 image: AssetImage(
                   "assets/a.png",
                 ),
-                fit: BoxFit.cover)),
+                fit: BoxFit.cover),
+          ),
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment(shipX, shipY),
+                child: Container(
+                  height: 80,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage("assets/ss.png")
+                      ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
