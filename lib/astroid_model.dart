1c9ecd5cb95ba38477393a1dfbe4dd6e185bebb9
Avinash175 <avinashmagar15@gmail.com>
2022-11-06 11:42:15 +0530

    New Code Adding

---
+++ lib/astroid_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class AstroidData {
+  final Size size;
+  Alignment alignment;
+  final String path="assets/astroid.png";
+
+  AstroidData({required this.alignment, required this.size});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
-
+import 'dart:math';
+import 'astroid_model.dart';
+import 'package:space_attackers/main.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,37 +35,95 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double shipX = 0.0, shipY = 0.0;
+  double maxHeight = 0.0;
+  double intialPosition = 0.0;
+  double velocity = 2.9;
+  double time = 0.0;
+  double gravity = -4.9;
+  bool isGameRunning = false;
 
-  double  shipX =0.0,shipY=0.0;
-  double maxHeight=0.0;
-  double intialPosition=0.0;
-  double velocity=2.9;
-  double time =0.0;
-  double gravity=-4.9;
-  bool isGameRunning =false;
+  List<AstroidData> astroidData = [];
+  List<AstroidData> setAstroidData() {
+    List<AstroidData> data = [
+      AstroidData(alignment: Alignment(2, 0.7), size: Size(40, 60),
+      ),
+      AstroidData(alignment: Alignment(1.5, -0.5), size: Size(80, 100),),
+      AstroidData(alignment: Alignment(3, -0.2), size: Size(40, 50),),
+      AstroidData(alignment: Alignment(2.2, 0.2), size: Size(60, 30),),
 
-  void startGame(){
-    isGameRunning=true;
+    ];
+
+    return data;
+  }
+
+  void startGame() {
+    isGameRunning = true;
     Timer.periodic(Duration(milliseconds: 30), (timer) {
-      time = time+ 0.02;
+      time = time + 0.02;
       setState(() {
-        maxHeight = velocity * time+ gravity *time * time;
-        shipY = intialPosition -maxHeight;
+        maxHeight = velocity * time + gravity * time * time;
+        shipY = intialPosition - maxHeight;
       });
+      moveAstroid();
     });
   }
 
-  void onJump(){
-   setState(() {
-     time =0;
-     intialPosition=shipY;
-   });
+  void onJump() {
+    setState(() {
+      time = 0;
+      intialPosition = shipY;
+    });
   }
+  double generateRandomNumber(){
+    Random rand =Random();
+    double randomDouble=rand.nextDouble() *(-1.0-1.0)+1.0;
+
+    return randomDouble;
+  }
+  void moveAstroid()
+  {
+    Alignment astroid1=astroidData[0].alignment;
+    Alignment astroid2=astroidData[1].alignment;
+    Alignment astroid3=astroidData[2].alignment;
+    Alignment astroid4=astroidData[3].alignment;
+
+  if (astroid1.x>-1.4){
+    astroidData[0].alignment=Alignment(astroid1.x -0.02,astroid1.y);
+  }else{
+    astroidData[0].alignment=Alignment(2, generateRandomNumber());
+  }
+
+    if (astroid2.x>-1.4){
+      astroidData[1].alignment=Alignment(astroid2.x -0.02,astroid1.y);
+    }else{
+      astroidData[1].alignment=Alignment(1.5, generateRandomNumber());
+    }
+
+    if (astroid3.x>-1.4){
+      astroidData[2].alignment=Alignment(astroid3.x -0.02,astroid1.y);
+    }else{
+      astroidData[2].alignment=Alignment(3, generateRandomNumber());
+    }
+
+    if (astroid4.x>-1.4){
+      astroidData[3].alignment=Alignment(astroid4.x -0.02,astroid1.y);
+    }else{
+      astroidData[3].alignment=Alignment(2.2, generateRandomNumber());
+    }
+
+  }
+
+  void initState(){
+    super.initState();
+    astroidData=setAstroidData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GestureDetector(
-        onTap: isGameRunning?onJump:startGame,
+        onTap: isGameRunning ? onJump : startGame,
         child: Container(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
@@ -79,11 +139,60 @@ class _MyHomePageState extends State<MyHomePage> {
               Align(
                 alignment: Alignment(shipX, shipY),
                 child: Container(
-                  height: 80,
-                  width: 100,
+                  height: 100,
+                  width: 130,
                   decoration: BoxDecoration(
-                      image: DecorationImage(image: AssetImage("assets/ss.png")
-                      ),
+                    image: DecorationImage(image: AssetImage("assets/ss.png")),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: astroidData[0].alignment,
+                child: Container(
+                  height: astroidData[0].size.height,
+                  width: astroidData[0].size.width,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage(astroidData[0].path))
+                  ),
+                ),
+              ),
+              Align(
+                alignment: astroidData[1].alignment,
+                child: Container(
+                  height: astroidData[1].size.height,
+                  width: astroidData[1].size.width,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage(astroidData[0].path))
+                  ),
+                ),
+              ),
+              Align(
+                alignment: astroidData[2].alignment,
+                child: Container(
+                  height: astroidData[2].size.height,
+                  width: astroidData[2].size.width,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage(astroidData[0].path))
+                  ),
+                ),
+              ),
+              Align(
+                alignment: astroidData[3].alignment,
+                child: Container(
+                  height: astroidData[3].size.height,
+                  width: astroidData[3].size.width,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage(astroidData[0].path))
+                  ),
+                ),
+              ),
+              Align(
+                alignment: astroidData[0].alignment,
+                child: Container(
+                  height: astroidData[0].size.height,
+                  width: astroidData[0].size.width,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage(astroidData[0].path))
                   ),
                 ),
               ),
